f542a4a237e076820da12f67e09538786009ab4f
sule <sule.yavuz.dev@gmail.com>
2025-07-23 12:03:59 +0300

    Initialize project with kurssitiedot exercises (osa1)

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+const Part = (props) => {
+  return <p>{props.name} {props.exercises}</p>
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1.name} exercises={props.part1.exercises} />
+      <Part name={props.part2.name} exercises={props.part2.exercises} />
+      <Part name={props.part3.name} exercises={props.part3.exercises} />
+    </div>
+  )
+}
+const Total = (props) => {
+  return (
+  <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+   
+  return (
+    <div>
+      <Header course={course}/>
+      <Content
+      part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
+     
+    </div>
+     
+  )
+}
+
+export default App
