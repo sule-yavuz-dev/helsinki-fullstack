1cf1ec09309cbc4ee76c810ca1f2c5b200325104
sule <sule.yavuz.dev@gmail.com>
2025-07-23 12:20:13 +0300

    Finish part 1.4 - refactor content to use map

--- src/App.jsx
+++ src/App.jsx
@@ -8,40 +8,41 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.part1.name} exercises={props.part1.exercises} />
-      <Part name={props.part2.name} exercises={props.part2.exercises} />
-      <Part name={props.part3.name} exercises={props.part3.exercises} />
+     {props.parts.map((part, index)=>(
+      <Part key={index} name={part.name} exercises={part.exercises} />
+     ))}
     </div>
   )
 }
+
 const Total = (props) => {
-  return (
-  <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
-  )
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return <p>Number of exercises {total}</p>
 }
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
    
   return (
     <div>
       <Header course={course}/>
-      <Content
-      part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts}/>
      
     </div>
      
